99869b9b6e3e1b05411a1e111b212ae8d8f73de9
Soujanya Koparagam <soujikkv@gmail.com>
2022-02-19 13:10:05 -0500

    added js to all

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function skemail() {
-   alert("My email is soujikkv@gmail.com")
+   alert("My email is sk@gmail.com")
 }
+function mattemail() {
+    alert("My email is mat@gmail.com")
+ }
+ function rajiemail() {
+    alert("My email is raji@gmail.com")
+ }
+ function joyemail() {
+    alert("My email is joy@gmail.com")
+ }
+ function danielleemail() {
+    alert("My email is dan@gmail.com")
+ }
